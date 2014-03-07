e60938262f6a5959fbae6d3edf93124f5928d1de
Ronald Kaiser <raios.catodicos@gmail.com>
2014-03-06 21:13:18 -0300

    first implementation

---
+++ speedreader.js
@@ -0,0 +1,24 @@
+id = undefined;
+pos = 0;
+text = undefined;
+tokens = undefined; 
+
+function update() {
+    if (pos >= tokens.length) {
+        pos = 0;
+    }
+    $("#current-word").text(tokens[pos++]);
+}
+
+$(document).ready(function(){
+
+    $("#start").click(function(){
+        text = $('#text').val();
+        tokens = text.split(' ');
+        id = setInterval(update, $("#timer").val());
+    });
+
+    $("#stop").click(function(){
+        clearInterval(id);
+    });
+});
